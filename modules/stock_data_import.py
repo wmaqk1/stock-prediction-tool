6ccc276ada1919b83cfd6e2fef93e65e5dc921bf
wmaqk1 <mateuszek.wojcik@gmail.com>
2024-11-12 22:15:19 +0100

    added data import

---
+++ modules/stock_data_import.py
@@ -0,0 +1,20 @@
+import yfinance
+import pandas
+
+# Define function to create table of all S&P 500 component stocks
+def import_table():
+    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+
+    stocks = pandas.read_html(url)
+    return stocks[0]
+
+# Define function to create table of historical stock data
+def import_stock_history():
+    stock_list = import_table()
+    data = []
+    for stock in stock_list.iterrows():
+        result = yfinance.download(stock['Symbol'], period='10y')
+        result['Symbol'] = stock['Symbol']
+        data.append(result)
+    final_list = pandas.concat(data)
+    return final_list
