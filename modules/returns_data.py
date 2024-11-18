02d3ed3275a3e45d791f0476620a7eff8b3cf7c5
wmaqk1 <mateuszek.wojcik@gmail.com>
2024-11-18 22:02:16 +0100

    added new feature and necesery comments

--- modules/returns_data.py
+++ modules/returns_data.py
@@ -1,10 +1,10 @@
-# Adds a return column for a specified time period.
 def add_returns(data_frame, period, label):
+    '''Add return column for a specified time period.'''
     data_frame[f'{label}_return'] = data_frame['Adj Close'] - data_frame['Adj Close'].shift(period)
     return data_frame
 
-# Adds statistics information for certain periods of time
 def add_rolling_statistic(data_frame, type_name):
+    '''Adds statistics information for certain periods of time.'''
     periods = [7, 21, 60, 100]
     for period in periods:
         data_frame[f'rolling_mean_{period}_days'] = data_frame[f'{type_name}'].rolling(window=period).mean()
@@ -13,24 +13,31 @@ def add_rolling_statistic(data_frame, type_name):
     return data_frame
 
 def daily_open_close_diff(data_frame):
+    '''Add daily difference between Open and Close prices.'''
     data_frame['Close-Open diff'] = data_frame['Close'] - data_frame['Open']
     return data_frame
 
-# Compute daily difference between lowest and highest price
 def daily_high_low_diff(data_frame):
+    """Compute daily difference between lowest and highest price."""
     data_frame['High-Low diff'] = data_frame['High'] - data_frame['Low']
     return data_frame
 
-# Compute the Relative Strength Index (RSI)
 def rsi_implementation(data_frame, window):
+    """Compute the Relative Strength Index (RSI)."""
     diff = data_frame['Close'].diff()
     gain = (diff.where(diff > 0, 0)).rolling(window=window).mean()
     loss = (-diff.where(diff < 0, 0)).rolling(window=window).mean()
     data_frame['rsi'] = 100 - (100 / (1 + gain/loss))
     return data_frame
 
-# Standard MACD calculation
+def create_lagged_features(df, lags=[1, 5, 21]):
+    """Add lagged features for stock prices."""
+    for lag in lags:
+        df[f'lag_{lag}'] = df['Close'].shift(lag)
+    return df
+
 def macd_implementation(data_frame):
+    """Add macd for stock prices."""
     data_frame['EMA_12'] = data_frame['Close'].ewm(span=12, adjust=False).mean()
     data_frame['EMA_26'] = data_frame['Close'].ewm(span=26, adjust=False).mean()
     data_frame['MACD'] = data_frame['EMA_12'] - data_frame['EMA_26']
@@ -38,6 +45,7 @@ def macd_implementation(data_frame):
     return data_frame
 
 def data_analysis(df):
+    """Add additional technical indicators do data frame."""
     periods = {
     'daily': 1,
     'weekly': 5,
@@ -60,12 +68,9 @@ def data_analysis(df):
 
     df['month'] = df.index.month
 
-    df['lag_21'] = df['Close'].shift(21)
+    df= create_lagged_features(df)
 
-    df['lag_21_rsi'] = df['rsi'].shift(21)
-
-    df = df.dropna()
     # Currently there is no acces to future predictions due to dropna usage
     df['test'] = df['Close'].shift(-21)
-    days_for_prediction = 21
-    return df, days_for_prediction
+    df = df.dropna()
+    return df
